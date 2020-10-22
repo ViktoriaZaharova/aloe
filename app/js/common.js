c5650f6d23dc1f44e912e11d14a830b506238a52
viktoriazaharova <zaharova181114@gmail.com>
2020-10-22 17:09:29 +0300

    add page

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+// maps
+ymaps.ready(function () {
+    var map = new ymaps.Map("map", {
+        center: [53.215742, 44.998526],
+        zoom: 16
+    });
+
+    var place1 = new ymaps.Placemark(
+        [53.215742, 44.998526], {
+            hintContent: '',
+        }, {
+            iconImageHref: 'img/location.svg',
+            iconImageSize: [91, 121],
+            iconLayout: 'default#image',
+        }
+    );
+
+
+    map.geoObjects.add(place1);
 });
